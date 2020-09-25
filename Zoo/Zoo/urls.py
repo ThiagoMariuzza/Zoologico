98436a242c5ee1730049c7fecff0e4ad54eea422
Thiago <thiagonicomb.xd@gmail.com>
2020-09-25 12:28:55 -0300

    nueva pag home.html

--- Zoo/Zoo/urls.py
+++ Zoo/Zoo/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('',include('Zoologico.urls')),
     url(r'^jet/', include('jet.urls', 'jet')),
     url(r'^admin/', admin.site.urls),
 ]
